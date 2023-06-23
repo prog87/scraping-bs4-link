03016d4d109a5ee2999c26388ec95840ce8212fc
Rizky Rahman <rizkpython87@gmail.com>
2023-06-23 23:30:38 +0800

    NoneType attribute

--- main.py
+++ main.py
@@ -1,36 +1,31 @@
-import requests
 from bs4 import BeautifulSoup
+import requests
 
-root = 'https://subslikescript.com'
-website = f'{root}/movies'
+#####################################################
+# Extracting the links of multiple movie transcripts
+#####################################################
+
+# How To Get The HTML
+root = 'https://subslikescript.com'  # this is the homepage of the website
+website = f'{root}/movies'  # concatenating the homepage with the movies section
 result = requests.get(website)
 content = result.text
 soup = BeautifulSoup(content, 'lxml')
-# print(soup.prettify())
+# print(soup.prettify())  # prints the HTML of the website
 
-box = soup.find('article', class_='main-article') # find() to get element
+# Locate the box that contains a list of movies
+box = soup.find('article', class_='main-article')
 
+# Store each link in "links" list (href doesn't consider root aka "homepage", so we have to concatenate it later)
 links = []
-for link in box.find_all('a', href=True):
+for link in box.find_all('a', href=True):  # find_all returns a list
     links.append(link['href'])
 
-print(links)
+#################################################
+# Extracting the movie transcript
+#################################################
 
-# for link in links:
-#     website = f'{root}/{link}'
-#     result = requests.get(website)
-#     content = result.text
-#     soup = BeautifulSoup(content, 'lxml')
-#
-#     box = soup.find('article', class_='main-article')
-#
-#     title = box.find('h1').get_text()
-#     title = ''.join(title.split('/'))
-#     transcript = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
-#
-#     with open(f'{title}.txt', 'w',encoding="utf-8") as file:
-#         file.write(transcript)
-
+# Loop through the "links" list and sending a request to each link
 for link in links:
     result = requests.get(f'{root}/{link}')
     content = result.text
@@ -39,7 +34,8 @@ for link in links:
     # Locate the box that contains title and transcript
     box = soup.find('article', class_='main-article')
     # Locate title and transcript
-    title = box
+    title = box.find('h1').get_text()
+    title = ''.join(title.split('/'))
     transcript = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
 
     # Exporting data in a text file with the "title" name
