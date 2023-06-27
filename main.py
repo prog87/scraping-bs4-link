c80dd578ea60e51705e96888b43d93acf18c09b7
Rizky Rahman <rizkpython87@gmail.com>
2023-06-27 21:11:29 +0800

    in module line 39

--- main.py
+++ main.py
@@ -36,21 +36,16 @@ for link in links:
     for e in soup.select('article', class_='main-article'):
         name = e.h1.get_text(strip=True)
         html = str(e)
-        file_title = open(name+'.txt', 'a', encoding='utf-8')
-        file_title.write(html)
-        file_title.close()
-#     # Locate title and transcript
-#     title = box.find('h1').get_text()
-#     title = ''.join(title.split('/'))
-#     transcript = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
-#
-#     # Exporting data in a text file with the "title" name
-#     with open(f'{title}.txt', 'w') as file:
-#         file.write(transcript)
-#
-# for e in soup.select('.colors a'):
-#     name = e.h2.get_text(strip=True)
-#     html = str(e)
-#     file = open(name+'.txt', 'a', encoding='utf-8')
-#     file.write(html)
-#     file.close()
+        with open(f'{name}.txt', 'w', encoding='utf-8') as file:
+            file.write(html)
+            file.close()
+    # Locate the box that contains title and transcript
+    # box = soup.select('article', class_='main-article')
+    # # Locate title and transcript
+    # title = box.find('h1').get_text()
+    # title = ''.join(title.split('/'))
+    # transcript = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
+    #
+    # # Exporting data in a text file with the "title" name
+    # with open(f'{title}.txt', 'w') as file:
+    #     file.write(transcript)
